25a472a44fa30e09b7b6355244daf8de97817738
EphraimPhiri <bsc-inf-23-19@unima.ac.mw>
2022-03-19 00:02:31 +0200

    added action event handler functions for all buttons

--- IotHouse.java
+++ IotHouse.java
@@ -7,65 +7,128 @@ import javafx.scene.layout.VBox;
 
 public class IotHouse extends Application implements RoomsDoors {
   @Override
-    public void start(Stage PrimaryStage){
-      
+  public void start(Stage PrimaryStage) {
 
-      rectangleOne.setStyle("-fx-stroke-width:5; -fx-stroke:BLACK; -fx-fill:grey");
-      rectangleTwo.setStyle("-fx-stroke-width:5; -fx-stroke:BLACK; -fx-fill:grey");
-      rectangleThree.setStyle("-fx-stroke-width:5; -fx-stroke:BLACK; -fx-fill:grey");
-      rectangleFour.setStyle("-fx-stroke-width:5; -fx-stroke:BLACK; -fx-fill:grey");
-      rectangleFive.setStyle("-fx-stroke-width:5; -fx-stroke:BLACK; -fx-fill:grey");
-      rectangleSix.setStyle("-fx-stroke-width:5; -fx-stroke:BLACK; -fx-fill:grey");
-      rectangleSeven.setStyle("-fx-stroke-width:5; -fx-stroke:BLACK; -fx-fill:grey");
+    rectangleOne.setStyle("-fx-stroke-width:5; -fx-stroke:BLACK; -fx-fill:grey");
+    rectangleTwo.setStyle("-fx-stroke-width:5; -fx-stroke:BLACK; -fx-fill:grey");
+    rectangleThree.setStyle("-fx-stroke-width:5; -fx-stroke:BLACK; -fx-fill:grey");
+    rectangleFour.setStyle("-fx-stroke-width:5; -fx-stroke:BLACK; -fx-fill:grey");
+    rectangleFive.setStyle("-fx-stroke-width:5; -fx-stroke:BLACK; -fx-fill:grey");
+    rectangleSix.setStyle("-fx-stroke-width:5; -fx-stroke:BLACK; -fx-fill:grey");
+    rectangleSeven.setStyle("-fx-stroke-width:5; -fx-stroke:BLACK; -fx-fill:grey");
 
-      button1.setOnAction(e -> {
-        if(rectangleOne.getStyle().contains("GREY")||rectangleTwo.getStyle().contains("GREY")||
-        rectangleThree.getStyle().contains("GREY")||rectangleFour.getStyle().contains("GREY")||
-        rectangleFive.getStyle().contains("GREY")||rectangleSix.getStyle().contains("GREY")||
-        rectangleSeven.getStyle().contains("GREY")){
-          rectangleOne.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:YELLOW");
-          rectangleTwo.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:GREEN");
-          rectangleThree.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:ORANGE");
-          rectangleFour.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:ORANGE");
-          rectangleFive.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:YELLOW");
-          rectangleSix.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:RED");
-          rectangleSeven.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:YELLOW"); 
-        }
-        else{
+    button1.setOnAction(e -> {
+      if (rectangleOne.getStyle().contains("GREY") && rectangleTwo.getStyle().contains("GREY") &&
+      rectangleThree.getStyle().contains("GREY") && rectangleFour.getStyle().contains("GREY") &&
+      rectangleFive.getStyle().contains("GREY") && rectangleSix.getStyle().contains("GREY") &&
+      rectangleSeven.getStyle().contains("GREY")) {
+    rectangleOne.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:YELLOW");
+    rectangleTwo.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:GREEN");
+    rectangleThree.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:ORANGE");
+    rectangleFour.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:ORANGE");
+    rectangleFive.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:YELLOW");
+    rectangleSix.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:RED");
+    rectangleSeven.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:YELLOW");
+  }
+      else {
         rectangleOne.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:GREY");
         rectangleTwo.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:GREY");
         rectangleThree.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:GREY");
         rectangleFour.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:GREY");
         rectangleFive.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:GREY");
         rectangleSix.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:GREY");
-        rectangleSeven.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:GREY"); 
-          
+        rectangleSeven.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:GREY");
+      }
+    });
+    button2.setOnAction(e -> {
+      if (rectangleOne.getStyle().contains("GREY") || rectangleTwo.getStyle().contains("GREY")) {
+        rectangleOne.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:YELLOW");
+        rectangleTwo.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:GREEN");
+      } else {
+        rectangleOne.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:GREY");
+        rectangleTwo.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:GREY");
+      }
+    });
+    button3.setOnAction(e ->{
+      if(rectangleOne.getStyle().contains("GREY")){
+        rectangleOne.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:YELLOW");
+      }
+      else{
+        rectangleOne.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:GREY");
+      }
+    });
+    button4.setOnAction(e ->{
+      if(rectangleTwo.getStyle().contains("GREY")){
+        rectangleTwo.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:GREEN");
+      }
+      else{
+        rectangleTwo.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:GREY");
+      }
+    });
+    button5.setOnAction(e ->{
+      if(rectangleThree.getStyle().contains("GREY")){
+        rectangleThree.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:ORANGE");
+      }
+      else{
+        rectangleThree.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:GREY");
+      }
+    });
+    button6.setOnAction(e ->{
+      if(rectangleFour.getStyle().contains("GREY")){
+        rectangleFour.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:ORANGE");
+      }
+      else{
+        rectangleFour.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:GREY");
+      }
+    });
+    button7.setOnAction(e ->{
+      if(rectangleFive.getStyle().contains("GREY")){
+        rectangleFive.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:YELLOW");
+      }
+      else{
+        rectangleFive.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:GREY");
+      }
+    });
+      button8.setOnAction(e ->{
+        if(rectangleSix.getStyle().contains("GREY")){
+          rectangleSix.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:RED");
+        }
+        else{
+          rectangleSix.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:GREY");
+        }
+      });
+      button9.setOnAction(e ->{
+        if(rectangleSeven.getStyle().contains("GREY")){
+          rectangleSeven.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:YELLOW");
+        }
+        else{
+          rectangleSeven.setStyle("-fx-stroke-width:4;-fx-stroke:black; -fx-fill:GREY");
         }
       });
 
-        //creating VBoxs that contain buttons
-         VBox vBox1 = new VBox(15,button1,button2, button3);
-         vBox1.setPadding(new Insets(500, 0, 0, 150));
-         VBox vBox2 = new VBox(15,button4,button5, button6);
-         vBox2.setPadding(new Insets(500, 0, 0, 350));
-         VBox vBox3 = new VBox(15,button7,button8,button9);
-         vBox3.setPadding(new Insets(500, 0, 0, 550));
+    // creating VBoxs that contain buttons
+    VBox vBox1 = new VBox(15, button1, button2, button3);
+    vBox1.setPadding(new Insets(500, 0, 0, 150));
+    VBox vBox2 = new VBox(15, button4, button5, button6);
+    vBox2.setPadding(new Insets(500, 0, 0, 350));
+    VBox vBox3 = new VBox(15, button7, button8, button9);
+    vBox3.setPadding(new Insets(500, 0, 0, 550));
 
-         //group to contain VBoxs that hold buttons
-      Group vItems = new Group(vBox3,vBox2,vBox1);
-      //group to contain rectangles that represent rooms
-      Group rectangleNodes = new Group(rectangleOne,rectangleTwo,
-      rectangleThree,rectangleFour,
-      rectangleFive,rectangleSix,
-      rectangleSeven);
-      //group to contain groups of rectangles and VBoxes
-      Group items = new Group (rectangleNodes,vItems);
+    // group to contain VBoxs that hold buttons
+    Group vItems = new Group(vBox3, vBox2, vBox1);
+    // group to contain rectangles that represent rooms
+    Group rectangleNodes = new Group(rectangleOne, rectangleTwo,
+        rectangleThree, rectangleFour,
+        rectangleFive, rectangleSix,
+        rectangleSeven);
+    // group to contain groups of rectangles and VBoxes
+    Group items = new Group(rectangleNodes, vItems);
 
-      Scene scene = new Scene(items, 900, 650);
-      PrimaryStage.setTitle("MyHouse");
-      PrimaryStage.setScene(scene);
-      PrimaryStage.show();
-    }
+    Scene scene = new Scene(items, 900, 650);
+    PrimaryStage.setTitle("MyHouse");
+    PrimaryStage.setScene(scene);
+    PrimaryStage.show();
+  }
 
   public static void main(String[] args) {
     Application.launch(args);
